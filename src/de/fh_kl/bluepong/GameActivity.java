69e4406a5ef7185bb4730c84ed8530576adf70b4
Lucien Buttel <lubu0001@stud.fh-kl.de>
2014-08-05 16:22:00 +0200

    added blank activity

---
+++ src/de/fh_kl/bluepong/GameActivity.java
@@ -0,0 +1,28 @@
+package de.fh_kl.bluepong;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.os.Build;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_game);
+	}
+}
--- src/de/fh_kl/bluepong/OnePlayerActivity.java
+++ src/de/fh_kl/bluepong/OnePlayerActivity.java
@@ -1,9 +1,11 @@
 package de.fh_kl.bluepong;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class OnePlayerActivity extends Activity {
 
@@ -11,5 +13,11 @@ public class OnePlayerActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_one_player);
+	}
+	
+	public void startGameActivity(View v) {
+		Intent i = new Intent(this, GameActivity.class);
+		
+		startActivity(i);
 	}
 }
