c74947fb298bde48e59a3dabdc20260beb54b015
David Friedrich <dafr0009@stud.fh-kl.de>
2014-08-05 14:50:33 +0200

    Initial Commit

---
+++ src/de/fh_kl/bluepong/MainActivity.java
@@ -0,0 +1,30 @@
+package de.fh_kl.bluepong;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void onePlayer(View v){
+		Intent intent = new Intent(this, OnePlayerActivity.class);
+		startActivity(intent);
+	}
+	
+	public void twoPlayer(View v){
+		Intent intent = new Intent(this, TwoPlayerActivity.class);
+		startActivity(intent);
+	}
+
+	public void tournamentMode(View v){	
+		Intent intent = new Intent(this, TournamentActivity.class);
+		startActivity(intent);
+	}
+}
